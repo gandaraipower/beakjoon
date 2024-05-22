0bfe77d19e33fbcc8a9081432d247077b7a079e9
park jongsoo <142289106+gandaraipower@users.noreply.github.com>
2024-05-22 22:25:32 +0900

    [Silver V] Title: 색종이, Time: 144 ms, Memory: 20856 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.kt
@@ -0,0 +1,26 @@
+fun main() {
+
+    val array1 = Array(100) { IntArray(100) { 0 } }
+    val input1 = readlnOrNull() ?: return
+    val num = input1.toInt()
+    var count=0
+    for (i in 0 until num) {
+        val input2 = readlnOrNull() ?: return
+        val num2 = input2.split(" ").map { it.toInt() }
+
+        for (j in 0 until 10) {
+            for (k in 0 until 10) {
+                array1[num2[0] + j][num2[1] + k] = 1    //1로 색칠
+            }
+        }
+    }
+
+    for(i in 0 until 100){
+        for (j in 0 until 100) {
+            if (array1[i][j] == 1)
+                count++ //1일때마다 카운트 상승
+        }
+    }
+
+    print(count)
+}
