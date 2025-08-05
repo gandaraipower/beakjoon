3ec52beae93968e60d5d37845e8d07b41fc2ee68
park jongsoo <142289106+gandaraipower@users.noreply.github.com>
2025-08-05 17:32:35 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.13 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        
+        for (int i = 0; i < my_string.length(); i++) {
+            char a = my_string.charAt(i);
+            if (Character.isDigit(a)) {
+                answer += Character.getNumericValue(a);
+            }
+        }
+        
+        return answer;
+    }
+}
